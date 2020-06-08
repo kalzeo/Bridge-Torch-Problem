19996a11cffdd98be4228aad0019701a64de12d8
Kyle McPherson <kylejmcpherson@gmail.com>
2020-06-09 00:47:51 +0100

    Update TestEquals.java

--- tests/TestEquals.java
+++ tests/TestEquals.java
@@ -12,11 +12,9 @@ public class TestEquals
 {
 	public static void main( String[] args )
     {
-		// Set up the initial and goal states as LinkedHashSets
 		LinkedHashSet<Person> westBank = new LinkedHashSet<Person>();
 		LinkedHashSet<Person> eastBank = new LinkedHashSet<Person>();
 		
-		// Add people to the western bank
 		westBank.add(new Person(1, "Adam"));   westBank.add(new Person(2, "Ben"));
 		westBank.add(new Person(5, "Claire")); westBank.add(new Person(8, "Doris"));
 		
@@ -28,7 +26,6 @@ public class TestEquals
 		State testDifferent = new BridgeState(westBank, eastBank, TorchDirection.WEST);
 		State testDifferent2 = new BridgeState(eastBank, westBank, TorchDirection.EAST);
 
-		// Print out if states are equal. True if equal otherwise false.
 		System.out.println("Are the following states equal?\n");
 		System.out.println("Is testSame Equal TO testSame2: " + testSame.equals(testSame2)); // true
 		System.out.println("Is testSame Equal TO testDifferent: " + testSame.equals(testDifferent)); // true
