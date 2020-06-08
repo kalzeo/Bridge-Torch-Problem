d4555f97c34f30cb07d54da8b1506de8305c08f4
Kyle McPherson <kylejmcpherson@gmail.com>
2020-06-09 00:48:12 +0100

    Update TestGoal.java

--- tests/TestGoal.java
+++ tests/TestGoal.java
@@ -14,19 +14,15 @@ public class TestGoal
 {
 	public static void main( String[] args )
     {
-		// Set up the initial and goal states as LinkedHashSets
 		LinkedHashSet<Person> westBank = new LinkedHashSet<Person>();
 		LinkedHashSet<Person> eastBank = new LinkedHashSet<Person>();
 		
-		// Add people to the west bank
 		westBank.add(new Person(1, "Adam"));   westBank.add(new Person(2, "Ben"));
 		westBank.add(new Person(5, "Claire")); westBank.add(new Person(8, "Doris"));
 		
-		// Create initial and goal states as BridgeStates
 		State initialState = new BridgeState(westBank, eastBank, TorchDirection.WEST);
 		State goalState = new BridgeState(westBank, eastBank, TorchDirection.EAST);
 		 
-		// Create a problem instance that will solve the solution using A*
 		SearchProblem problem = new Astar(initialState, goalState);
 		
 		// Print out if the current state is the goal state. True if goal, false if not.
